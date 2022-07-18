60df9de655bfb836ccc403041fb3ba3da31e4e40
shilpaahire <108166443+shilpaahire@users.noreply.github.com>
2022-07-19 00:18:28 +0530

    Add files via upload

---
+++ project11/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { Display1Component } from './display1/display1.component';
+// import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    Display1Component
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    // FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ project11/src/app/display1/display1.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-display1',
+  templateUrl: './display1.component.html',
+  styleUrls: ['./display1.component.css']
+})
+export class Display1Component implements OnInit {
+  frmClient:any;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.frmClient =  new FormGroup({
+      clientid: new FormControl("",[Validators.required, Validators.minLength(6)]),
+      clientname: new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+      address:new FormControl("", Validators.required),
+      phoneno: new FormControl("", Validators.pattern('^[0-9]+$'))
+    })
+  }
+  onSubmit() {
+    console.log("Submited", this.frmClient.status);
+  }
+
+}
